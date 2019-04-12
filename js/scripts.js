ff9c32a4795f42e0ccfca4fd3d683db11a39e8e5
Mathew Akre <mwakre29@gmail.com>
2019-04-12 11:06:30 -0700

    add user interface

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 // back end
 var pizzas = [];
 
-function Pizza(size,) {
-  this.size = size
+function Pizza(size, toppings) {
+  this.size = size,
+  this.addToppings(toppings)
 }
 
 function Size(name, price) {
-  this.name = name
-  this.price
+  this.name = name,
+  this.price = price
 }
 
 function Toppings(name, price) {
@@ -16,17 +17,56 @@ function Toppings(name, price) {
 }
 
 Pizza.prototype.addToppings = function(toppings) {
-  var topOnPizza = []
+  var topOnPizza = [];
   toppings.forEach(function(toppping) {
     topOnPizza.push(topping);
   });
   this.toppings = topOnPizza;
 }
 
-Pizza.prototpe.getPrice = function() {
+Pizza.prototype.getPrice = function() {
   price = parseInt(this.size.price)
   this.toppings.forEach(function(topping) {
-    price ++ (parseInt(top.price)/100)
-  })
+    price += (parseInt(topping.price)/100)
+  });
   return price;
 }
+
+
+
+
+
+
+
+// user interface
+var pizza = new Pizza();
+
+function displayOrderDetails() {
+  var orderList = $("ul#order-list");
+  var htmlForOrderInfo = "";
+  for(var i = 0; i < pizzas.length; i++){
+    htmlForOrderInfo += "<li id=" + i + ">" + pizzas[i]size.name + " " + pizzas[i].toppings.length + " topping pizza</li>";
+  };
+  orderList.html(htmlForOrderInfo);
+};
+
+function attachListeners() {
+ $("#")
+};
+
+$(document).ready(function() {
+  attachListeners();
+  $("form#make-pizza").submit(function(event) {
+    event.preventDefault();
+    var selectedSize = $("#size option:selected").text();
+    var inputtedToppings = $("input:checkbox[name=add-toppings]:checked").val();
+    var size = new Size(selectedSize, $("#size").val());
+    var topping = []
+
+
+    });
+
+
+  });
+
+});
